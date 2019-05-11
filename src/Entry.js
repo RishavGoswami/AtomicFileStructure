94aeb0b5ebd6c4bdd64d556d941f4a97632794c8
Rishav <Rishav.goswami@prontoitlabs.com>
2019-05-11 12:15:10 +0530

    Let's go

---
+++ src/Entry.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import HomePage from "./components/templates/HomePage";
+
+class Entry extends Component {
+  render() {
+    return 'Let\'s go';
+  }
+}
+
+export default Entry;
